81fd8a06960635214c7b58f57da405f959a1aa27
hoangpham96 <hoangpham96@gmail.com>
2017-06-07 14:47:40 +1000

    Create cookie

--- data/testLogin.js
+++ data/testLogin.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
    $("#u185").click(function(){
-   		alert("Login");
+   		var username = $("#u186_input").val();
+   		var password = $("#u187_input").val();
+   		alert("Login: "+username+" "+password);
+   		createCookie("username",username,1);
+	   		// window.localStorage.setItem("username",username);
    });
-
 });
