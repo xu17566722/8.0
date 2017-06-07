f969ee7f652cc098c5b8c33c860eaa278b7cab53
xu17566722 <xu17566722@hotmail.com>
2017-06-07 13:59:55 +0800

    Pseudo-login Functionality

--- data/cookieHandler.js
+++ data/cookieHandler.js
@@ -1,25 +1,20 @@
-function createCookie(name, value, days) {
+var createCookie = function(name, value, days) {
     var expires;
-
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toGMTString();
-    } else {
+    }
+    else {
         expires = "";
     }
-    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-function readCookie(name) {
-    var nameEQ = encodeURIComponent(name) + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    return null;
+function getCookie(name) {
+	  var value = "; " + document.cookie;
+	  var parts = value.split("; " + name + "=");
+	  if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
 function eraseCookie(name) {
--- data/testLogin.js
+++ data/testLogin.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
-
+	var loggedIn = false;
+	if (getCookie("username")!=null){
+		loggedIn = true;
+	}
+	
+	function showUserPage(){
+	   if (loggedIn) {
+		   var username = getCookie("username")
+		   console.log("Logged In!" + username);
+	  		//44,186,46,187,184 is code for Uname, Uname Input, Pw, Pw input, login
+	  		$("#loginSect").replaceWith("<div id='u46' class='ax_default box_2'><div id='u46_div' class=''></div><div id='u47' class='text' style='visibility: visible;'><p><span>"+"Welcome, "+username+" !"+"</span></p></div></div>");
+	  }
+	}
+	
+	showUserPage();
    $("#u185").click(function(){
    		var username = $("#u186_input").val();
    		var password = $("#u187_input").val();
+   		
+   		//TODO: verify username and password with the server
    		alert("Login: "+username+" "+password);
+   		
    		createCookie("username",username,1);
-	   		// window.localStorage.setItem("username",username);
+   		loggedIn = true;
+   		showUserPage();
    });
+
+	eraseCookie("username");
+
 });
